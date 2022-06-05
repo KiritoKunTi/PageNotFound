943e00f4252768c1d82fbdafec018e943798640e
duman <duman200354@gmail.com>
2022-06-05 12:39:40 +0600

    scroll reveal js added to content

--- js/script.js
+++ js/script.js
@@ -9,3 +9,12 @@ showMenuBtn.addEventListener('click', () => {
 closeMenuBtn.addEventListener('click', () => {
     nodeMenu.style.top = "-100%";
 })
+
+const scr = ScrollReveal({
+    distance: '90px',
+    duration: 3000,
+});
+
+scr.reveal(`.content`, {origin: 'top', delay: '100'});
+scr.reveal(`.ghost-picture`, {origin: 'bottom', delay: '200'});
+scr.reveal(`.footer`, {origin: 'bottom', delay: '200'});
